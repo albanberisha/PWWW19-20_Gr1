4d6f70c08e1322cdbcc66d4c14a28d9d87165d22
ArtiSadikaj <37617960+ArtiSadikaj@users.noreply.github.com>
2020-01-04 23:22:53 +0100

    Add files via upload

--- Projekti/kodi.php
+++ Projekti/kodi.php
@@ -3,7 +3,17 @@
 <head>
 	<title></title>
 </head>
-<body>
-<?php echo $_POST['name']; ?>
+<body style="background-image: url(images/back1.jpg); margin:20px;">
+<?php
+ echo "Keni kerkuar punetorin me id ".$_POST['id'].".</br>"  ; 
+ echo "<a href='punetoret.html' style='text-decoration:none; color:blue'>Shiko orarin e punetoreve</a>";
+?>
+
+
+
+<p>Me poshte do ta shifni punetorin e kerkuar:</p>
+<p style="border: 1px solid black; margin-right:525px;">
+<img src="images/punetori.jpg" alt="Fotoja nuk eshte ne dizposicion per shkak te privatesise." style="  width:200px; height:200px;">
+</p>
 </body>
 </html>
